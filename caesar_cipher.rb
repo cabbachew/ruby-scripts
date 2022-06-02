fb9a6ebd598d6cda448c3cba987333c0ed4f2549
Daniel Oh <cabbachew@gmail.com>
2022-06-02 20:06:03 +0900

    Complete caesar_cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -3,12 +3,27 @@
 #   A = 65, Z = 90, a = 97, z = 122
 # - wrap z to a
 # - keep the same case
+# ? Check for valid key
 
 def caesar_cipher(string, key)
-
+  puts cipher_text = encode(string.split(""), key).join("")
+  puts cipher_text.class
 end
 
-# caesar_cipher("What a string!", 5)
-# puts ""
-# caesar_cipher("Hello world!", 3)
-# puts ""
+def encode(arr, key)
+  arr.map! do |char|
+     if !!/[[:lower:]]/.match(char)
+       char = ((((char.ord - 97) + key) % 26) + 97).chr
+     elsif !!/[[:upper:]]/.match(char)
+       char = ((((char.ord - 65) + key) % 26) + 65).chr
+     else
+      char
+     end
+  end
+  arr # Return array
+end
+
+caesar_cipher("What a string!", 5)
+puts ""
+caesar_cipher("Hello world!", 3)
+puts ""
