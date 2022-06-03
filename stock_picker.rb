f9a82c2618622e74ca7117b914dda26b1529f800
Daniel Oh <cabbachew@gmail.com>
2022-06-03 20:45:57 +0900

    Update notes for stock_picker

--- stock_picker.rb
+++ stock_picker.rb
@@ -4,6 +4,7 @@
 # * Days start at 0
 # * You need to buy before you can sell
 # * Edge cases: lowest day is last day OR highest day is first day
+# ? Alternative method: find maximum sub subarray of differences
 
 def stock_picker(array)
   p sorted_prices = array.each_with_index.max(array.length)
